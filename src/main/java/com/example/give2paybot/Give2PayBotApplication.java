5f571ebb43237a019a8a49b1d541b0255a190fb7
bahram <bahram.barati.job@gmail.com>
2022-07-23 22:21:35 +0430

    start project

---
+++ src/main/java/com/example/give2paybot/Give2PayBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.give2paybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Give2PayBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Give2PayBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/give2paybot/Give2PayBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.give2paybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Give2PayBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
